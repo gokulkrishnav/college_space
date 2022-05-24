d7baf54071daab59564b967ca0161d0f5543a367
Asus <Asus@GOKULKRISHNAV>
2022-05-24 16:59:57 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace college_space.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
